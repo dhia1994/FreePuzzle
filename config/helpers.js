d94ce60e406a38f88feb876595a26415ab995f78
mdbousarsar <mohameddhia.boussarsar@technica-engineering.de>
2021-11-08 23:31:27 +0100

    [DhBo] : commit changes changes for phone version screen

--- config/helpers.js
+++ config/helpers.js
@@ -6,7 +6,7 @@ let conn = new Mysqli({
     post: 3306, // Port, default 3306  
     user: 'root', // Username  
     passwd: '', // password  
-    db: 'TreePuzzle' //You  can specify the database or not [optional]  
+    db: 'FreePuzzle' //You  can specify the database or not [optional]  
 });
 
 let db = conn.emit(false, '')
