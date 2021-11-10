a18c0f2c597fa1c6abfdb2c9e092f56b5039a9fe
mdbousarsar <mohameddhia.boussarsar@technica-engineering.de>
2021-11-10 19:03:15 +0100

    [DhBo] : commit changes helpers... :'( :D (y) :'''(....

--- config/helpers.js
+++ config/helpers.js
@@ -1,20 +1,24 @@
-const { Prohairesis } = require('prohairesis');
-/*
 const Mysqli = require('mysqli');
  
 // Pass in json
-let conn = new Mysqli({
-    host: 'us-cdbr-east-04.cleardb.com', //  IP/domain name  
+let conn;
+if (process.env.CLEARDB_DATABASE_URL) {
+    conn = new Mysqli(process.env.CLEARDB_DATABASE_URL)
+}
+else {
+    conn = new Mysqli({
+    host: 'localhost', //  IP/domain name  
     post: 3306, // Port, default 3306  
-    user: 'bad3b98f4973cc', // Username  
-    passwd: 'e042c3ac', // password  
-    db: 'heroku_3df33406a96dc2a' //You  can specify the database or not [optional]  
+    user: 'root', // Username  
+    passwd: '', // password  
+    db: 'freePuzzle' //You  can specify the database or not [optional]  
 });
-*/
 
-// create new database instance
-let db = new Prohairesis("mysql://bad3b98f4973cc:e042c3ac@us-cdbr-east-04.cleardb.com/heroku_3df33406a96dc2a?reconnect=true");
-//db = conn.emit(false, '')
+}
+
+
+
+db = conn.emit(false, '')
 module.exports = {
     database : db
 }
--- server.js
+++ server.js
@@ -42,6 +42,6 @@ app.get('/*', (req, res) =>
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000);
 
 module.exports = app;
