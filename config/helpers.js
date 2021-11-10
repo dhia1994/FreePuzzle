f356e4d66bee95acc8a7088d91d05ce9c6d6c0ff
mdbousarsar <mohameddhia.boussarsar@technica-engineering.de>
2021-11-10 17:27:32 +0100

    [DhBo] : commit changes for helper.js

--- config/helpers.js
+++ config/helpers.js
@@ -2,11 +2,11 @@ const Mysqli = require('mysqli');
  
 // Pass in json
 let conn = new Mysqli({
-    host: 'localhost', //  IP/domain name  
+    host: 'us-cdbr-east-04.cleardb.com', //  IP/domain name  
     post: 3306, // Port, default 3306  
-    user: 'root', // Username  
-    passwd: '', // password  
-    db: 'FreePuzzle' //You  can specify the database or not [optional]  
+    user: 'bad3b98f4973cc', // Username  
+    passwd: 'e042c3ac', // password  
+    db: 'heroku_3df33406a96dc2a' //You  can specify the database or not [optional]  
 });
 
 let db = conn.emit(false, '')
