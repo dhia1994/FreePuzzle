39b8e581c131539b4522a53868848074236563a1
mdbousarsar <mohameddhia.boussarsar@technica-engineering.de>
2021-11-10 22:34:19 +0100

    [DhBo] : commit changes server again and again and again and agaiiiiiin

--- config/helpers.js
+++ config/helpers.js
@@ -1,22 +1,18 @@
 const Mysqli = require('mysqli');
- 
+
 let conn;
-process.env.CLEARDB_DATABASE_URL = 'mysql://bad3b98f4973cc:e042c3ac@us-cdbr-east-04.cleardb.com/heroku_3df33406a96dc2a?reconnect=true';
-if (process.env.CLEARDB_DATABASE_URL) {
-    conn = new Mysqli(process.env.CLEARDB_DATABASE_URL)
-}
-else {
-    // Pass in json
-    conn = new Mysqli({
-        host: 'localhost', //  IP/domain name  
-        post: 3306, // Port, default 3306  
-        user: 'root', // Username  
-        passwd: '', // password  
-        db: 'FreePuzzle' //You  can specify the database or not [optional]  
-    });
-}
+
+// Pass in json
+conn = new Mysqli({
+  host: 'us-cdbr-east-04.cleardb.com', //  IP/domain name  
+  post: 3306, // Port, default 3306  
+  user: 'bad3b98f4973cc', // Username  
+  passwd: 'e042c3ac', // password  
+  db: 'heroku_3df33406a96dc2a' //You  can specify the database or not [optional]  
+});
+
 let db = conn.emit(false, '')
 
 module.exports = {
-    database : db
+  database: db
 }
